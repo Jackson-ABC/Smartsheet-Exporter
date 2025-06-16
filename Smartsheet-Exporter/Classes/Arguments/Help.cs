56ca8c7edff3dffa7aea4f793b1081ff52c5a2e1
Jackson-ABC <jackson@abcsheds.net.au>
2025-06-16 13:29:07 +1000

    Update help arg
    Add handling to return help for specified commands

--- Smartsheet-Exporter/Classes/Arguments/Help.cs
+++ Smartsheet-Exporter/Classes/Arguments/Help.cs
@@ -2,7 +2,7 @@
 {
     internal static class Help
     {
-        public static bool Handler(
+        public static string Handler(
             string[] args,
             out string? parsedInputFilePath,
             out string? parsedFileType,
@@ -13,21 +13,49 @@
             parsedInputFilePath = null;
             parsedFileType = null;
             parsedOutputDir = null;
+            outputText = "";
 
-            outputText = "Usage: SmartsheetExporter.exe <arguments>\n";
-            outputText += "Example: SmartsheetExporter.exe --token <api_token> --outputPath <some_dir_path>\n";
-            outputText += "<api_token> should be a generated smartsheet api token (see https://help.smartsheet.com/articles/2482389-generate-API-key)\n";
-            outputText += "\n";
-            outputText += "Available arguments:\n";
-
-            foreach (var command in ArgumentsHandler.Commands)
+            if(args.Length > 1)
             {
-                outputText += $"{command.Value.Key}: {command.Value.Description}\n";
-                outputText += $"  Aliases: {command.Value.Aliases}\n";
-                outputText += "\n";
+                string knownCommands = "Argument Help:\n";
+                knownCommands += "==============\n";
+
+                string unknownCommands = "Unknown Commands:\n";
+                unknownCommands += "=================\n";
+
+                foreach (string arg in args)
+                {
+                    if (ArgumentsHandler.Commands.TryGetValue(arg, out Command? command))
+                    {
+                        knownCommands += $"{command.Key} : {command.Description}\n";
+                        knownCommands += $"    Aliases: {command.Aliases}\n";
+                        knownCommands += $"\n";
+                    }
+                    else if (arg.StartsWith('-'))
+                        unknownCommands += $"{arg}\n";
+                }
+
+                if (knownCommands.Length > 0 || unknownCommands.Length > 0)
+                    outputText = knownCommands + unknownCommands;
             }
 
-            return false;
+            if (outputText.Length == 0)
+            {
+                outputText += "Usage: SmartsheetExporter.exe <arguments>\n";
+                outputText += "Example: SmartsheetExporter.exe --token <api_token> --outputPath <some_dir_path>\n";
+                outputText += "<api_token> should be a generated smartsheet api token (see https://help.smartsheet.com/articles/2482389-generate-API-key)\n";
+                outputText += "\n";
+                outputText += "Available arguments:\n";
+
+                foreach (var command in ArgumentsHandler.Commands)
+                {
+                    outputText += $"{command.Value.Key}: {command.Value.Description}\n";
+                    outputText += $"  Aliases: {command.Value.Aliases}\n";
+                    outputText += "\n";
+                }
+            }
+
+            return outputText;
         }
     }
 }
