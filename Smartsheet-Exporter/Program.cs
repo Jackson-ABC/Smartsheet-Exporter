c10c213f25bee732198f5838f1bef0aa0cc37e9b
Jackson-ABC <jackson@abcsheds.net.au>
2025-10-01 09:07:50 +1000

    [WIP][C#][CLI] feat(program): use readline arguments temporarily

--- Smartsheet-Exporter/Program.cs
+++ Smartsheet-Exporter/Program.cs
@@ -1,4 +1,7 @@
-﻿using Smartsheet_Exporter.Classes;
+﻿using Smartsheet.Api;
+using Smartsheet.Api.Models;
+using Smartsheet_Exporter.Classes;
+using System.Security.Cryptography;
 
 namespace Smartsheet_Exporter
 {
@@ -11,11 +14,30 @@ namespace Smartsheet_Exporter
             Console.WriteLine("A tool for exporting data from smartsheet to enable tracking in git");
             Console.WriteLine();
 
-            if (!ArgumentsHandler.HandleArguments(args, out string? inputFilePath, out string? fileType, out string? outputDir, out string? outputText))
+            Console.WriteLine("Input Smartsheet API Key:");
+            string apiKey = Console.ReadLine() ?? "";
+
+            Console.WriteLine("Input Smartsheet sheet id:");
+            string sheetID_str = Console.ReadLine() ?? "";
+
+            Console.WriteLine("Input save location:");
+            string savePath = Console.ReadLine() ?? "";
+
+            long sheetID = long.Parse(sheetID_str);
+
+            SmartsheetClient client = new SmartsheetBuilder()
+                .SetAccessToken(apiKey)
+                .Build();
+
+            Sheet sheet = client.SheetResources.GetSheet(sheetID);
+            
+            ExportHandler.Go(sheet, savePath);
+
+            /*if (!ArgumentsHandler.HandleArguments(args, out string? inputFilePath, out string? fileType, out string? outputDir, out string? outputText))
             {
                 Console.WriteLine(outputText);
                 return;
-            }
+            }*/
         }
     }
 }
